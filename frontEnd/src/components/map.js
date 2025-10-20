2d43d86bb4c98c223a7a2264509c2f00bb395566
mojmirko <mojmir126@gmail.com>
2025-10-20 09:48:32 +0200

    push

--- frontEnd/src/components/map.js
+++ frontEnd/src/components/map.js
@@ -12,10 +12,9 @@ L.Icon.Default.mergeOptions({
 });
 
 const redIcon = L.icon({
-  iconUrl:
-    'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+  iconUrl: "/assets/icon.png",
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
-  iconSize: [25, 41],
+  iconSize: [45, 50],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   shadowSize: [41, 41],
