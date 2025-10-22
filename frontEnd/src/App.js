33ebaef5f48a021bc69506e6ac9b43f5e4131092
martinrutai <rutai.martin@gmail.com>
2025-10-22 08:44:04 +0200

    Opel Astra 1.6 i Cosmo

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -2,8 +2,15 @@ import './App.css';
 import Map from './components/map.js';
 import StatBar from './components/statusBar.js';
 import { UserProvider } from './components/ContextUser.js';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:8081/cars')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log('Error fetching cars:', err));
+  }, []);
   return (
     <UserProvider>
       <body style={{backgroundColor: '#353535ff', overflow: 'hidden', marginTop: '0vh'}}>
